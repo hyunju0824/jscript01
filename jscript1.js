f130b059411af263f4ff3946fedbbb42259b4071
hyunju0824 <112843545+hyunju0824@users.noreply.github.com>
2022-10-17 17:44:27 +0900

    Add files via upload

---
+++ jscript1.js
@@ -0,0 +1,6 @@
+        function hide(){
+            document.getElementById("demo").style.display = 'none';
+        }
+        function show(){
+            document.getElementById("demo").style.display = 'block';
+        }
